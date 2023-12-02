f4fffc4f5f241b4bab125b9031f84e4a47863cb1
Antoine Busch <antoine.busch@gmail.com>
2023-12-02 13:35:38 +1100

    Simplify a bit and use `char::to_digit()`

--- src/day01.rs
+++ src/day01.rs
@@ -3,36 +3,36 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::satisfy,
-    combinator::{map, value},
+    combinator::{map_res, value},
     IResult,
 };
 
 pub fn run() -> Result<()> {
     let data = std::fs::read_to_string("inputs/day01.txt")?;
-    let total: u64 = data
+    let total: u32 = data
         .lines()
         .map(|line| {
             let digits = line
-                .bytes()
-                .filter_map(|c| c.is_ascii_digit().then_some(c - b'0'))
+                .chars()
+                .filter_map(|c| c.to_digit(10))
                 .collect::<Vec<_>>();
-            let d1 = digits.first().copied().unwrap() as u64;
-            let d2 = digits.last().copied().unwrap() as u64;
+            let d1 = digits.first().copied().expect("Line has no digits!");
+            let d2 = digits.last().copied().expect("Line has no digits!");
             d1 * 10 + d2
         })
         .sum();
 
     println!("Part1: {total}");
 
-    let total: u64 = data
+    let total: u32 = data
         .lines()
         .map(|line| {
             let bytes = line.as_bytes();
             let digits = (0..bytes.len())
                 .filter_map(|i| parse_digit(&bytes[i..]).ok().map(|(_rest, digit)| digit))
                 .collect::<Vec<_>>();
-            let d1 = digits.first().copied().unwrap() as u64;
-            let d2 = digits.last().copied().unwrap() as u64;
+            let d1 = digits.first().copied().expect("Line has no digits!");
+            let d2 = digits.last().copied().expect("Line has no digits!");
             d1 * 10 + d2
         })
         .sum();
@@ -42,7 +42,7 @@ pub fn run() -> Result<()> {
     Ok(())
 }
 
-fn parse_digit(input: &[u8]) -> IResult<&[u8], u8> {
+fn parse_digit(input: &[u8]) -> IResult<&[u8], u32> {
     alt((
         value(1, tag(b"one")),
         value(2, tag(b"two")),
@@ -53,6 +53,8 @@ fn parse_digit(input: &[u8]) -> IResult<&[u8], u8> {
         value(7, tag(b"seven")),
         value(8, tag(b"eight")),
         value(9, tag(b"nine")),
-        map(satisfy(|c| c.is_ascii_digit()), |c| (c as u8 - b'0')),
+        map_res(satisfy(|c| c.is_ascii_digit()), |c| {
+            c.to_digit(10).ok_or("Char should be a digit")
+        }),
     ))(input)
 }
