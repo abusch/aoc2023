549c6fab92fab29b255dfcf4fa873c7d61592942
Antoine Busch <antoine.busch@gmail.com>
2023-12-12 18:31:38 +1100

    Day 09 part 2

--- src/day09.rs
+++ src/day09.rs
@@ -1,5 +1,5 @@
 use aoc2023::Day;
-use color_eyre::{eyre::bail, Result};
+use color_eyre::Result;
 use nom::{bytes::complete::tag, character::complete::i64, multi::separated_list1, IResult};
 
 inventory::submit! {
@@ -17,8 +17,13 @@ fn part1(input: &str) -> Result<String> {
 }
 
 fn part2(input: &str) -> Result<String> {
-    // Ok(format!("{count}"))
-    bail!("todo")
+    let sequences = input
+        .lines()
+        .map(|line| parse_history(line).expect("invalid input").1)
+        .collect::<Vec<_>>();
+
+    let total: i64 = sequences.into_iter().map(|seq| extrapolate2(&seq)).sum();
+    Ok(format!("{total}"))
 }
 
 fn extrapolate(seq: &[i64]) -> i64 {
@@ -34,6 +39,22 @@ fn extrapolate(seq: &[i64]) -> i64 {
     extrapolate_inner(seq, 0)
 }
 
+fn extrapolate2(seq: &[i64]) -> i64 {
+    fn extrapolate_inner(seq: &[i64], sum: i64, factor: i64) -> i64 {
+        if seq.iter().all(|v| *v == 0) {
+            sum
+        } else {
+            let diffs = seq.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+            extrapolate_inner(
+                &diffs,
+                sum + factor * seq.first().copied().unwrap(),
+                -factor,
+            )
+        }
+    }
+
+    extrapolate_inner(seq, 0, 1)
+}
 fn parse_history(input: &str) -> IResult<&str, Vec<i64>> {
     separated_list1(tag(" "), i64)(input)
 }
@@ -46,5 +67,11 @@ mod tests {
     fn test_extrapolate() {
         assert_eq!(extrapolate(&[1, 3, 6, 10, 15, 21]), 28);
         assert_eq!(extrapolate(&[10, 13, 16, 21, 30, 45]), 68);
+    }
+
+    #[test]
+    fn test_extrapolate2() {
+        assert_eq!(extrapolate2(&[1, 3, 6, 10, 15, 21]), 0);
+        assert_eq!(extrapolate2(&[10, 13, 16, 21, 30, 45]), 5);
     }
 }
